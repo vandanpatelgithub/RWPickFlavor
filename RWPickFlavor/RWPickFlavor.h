d864d63d69011fca7dd0a7b1c614efa6aacf2696
Vandan Patel <vandan.patel@nike.com>
2018-08-22 19:58:28 -0700

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Patel, Vandan (ETW - FLEX) on 8/22/18.
+//  Copyright © 2018 Patel, Vandan (ETW - FLEX). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
